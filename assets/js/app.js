2ff95ba0ab9aacc26c88e0aeb4e77e551f592d7d
Floris van Drunen <floris.van.drunen@outlook.com>
2026-01-09 12:28:33 +0100

    redoing it cus copilot is not that good

--- assets/js/app.js
+++
@@ -1,5 +0,0 @@
-// Simple JS starter for the Vinyl Website
-document.addEventListener('DOMContentLoaded', function(){
-  console.log('Vinyl Website: app.js loaded');
-  // TODO: Add interactive behaviour (menu, modals, etc.)
-});
---
+++ pages/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body>
+    <div class="main-container">
+        <?php include '../includes/header.php'; ?>
+
+        <h1>Welcome to Vinyl Hackathon</h1>
+        <p>Your one-stop destination for all things vinyl!</p>
+            
+        <?php include '../includes/footer.php'; ?>
+    </div>
+</body>
+
+</html>
